812d0e5705fcc94ade3040c2574a9b5cf9194eca
wangzhuoyue <2315753700@qq.com>
2020-09-08 15:19:08 +0800

    first\

--- 1906101031王卓越/src/Hello_World.java
+++ 1906101031王卓越/src/Hello_World.java
@@ -1,7 +1,5 @@
-package src;
-
 public class Hello_World {
     public static void main(String[] args) {
-        System.out.println("Hello,World");
+        System.out.println("hello，world");
     }
 }
