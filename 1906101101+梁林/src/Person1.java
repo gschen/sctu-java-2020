6f6a406643ff6dfa1f3d24d76a7d9d305a36b7c1
llworldking <2812416175@qq.com>
2020-09-13 10:18:09 +0800

    work

---
+++ 1906101101+梁林/src/Person1.java
@@ -0,0 +1,23 @@
+public class Person1 {
+
+
+    void study() {
+        System.out.println('s');
+
+    }
+
+    void sport() {
+
+    }
+
+    public static void main(String[] args) {
+        String name = "小明";
+        int age = 99;
+        String hobby = "滑雪";
+        String friend = "小花";
+        String home = "和平路";
+        System.out.println(home);
+        Person1 a = new Person1();
+        a.study();
+    }
+}
