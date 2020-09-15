2a2f9fe093a4739dda56e52a0e0e798914f36be8
lihelong1 <857788573@qq.com>
2020-09-15 15:35:12 +0800

    第二周第三次课

--- 1906101007李和龙/src/edu/sctu/java/day0908/Person.java
+++ 1906101007李和龙/src/edu/sctu/java/day0908/Person.java
@@ -1,3 +1,5 @@
+package edu.sctu.java.day0908;
+
 public class Person {
         String name = "张三";
         int age = 18;
--- 1906101007李和龙/src/edu/sctu/java/day0908/test.java
+++ 1906101007李和龙/src/edu/sctu/java/day0908/test.java
@@ -1,3 +1,5 @@
+package edu.sctu.java.day0908;
+
 public class test {
     public static void main(String[] args){
         System.out.print("hello world");
