3d7a6d2c3f331be11ed909b7e4c987f702b6e2e6
lihelong1 <857788573@qq.com>
2020-09-16 11:56:20 +0800

    第三周第三次课

---
+++ 1906101007李和龙/src/edu/sctu/java/day0915/TestThree.java
@@ -0,0 +1,12 @@
+package edu.sctu.java.day0915;
+
+import edu.sctu.java.day0916.TestOne;
+
+public class TestThree extends TestOne {
+    public static void main(String[] args) {
+        TestThree chen = new TestThree();
+        chen.name = "chen";
+        chen.sexy = "male";
+        chen.age = 16;
+    }
+}
---
+++ 1906101007李和龙/src/edu/sctu/java/day0916/TestTwo.java
@@ -0,0 +1,19 @@
+package edu.sctu.java.day0916;
+
+public class TestTwo {
+    public static void main(String[] args) {
+        TestOne li = new TestOne();
+        li.age = 16;
+        li.name = "li";
+        li.sexy = "male";
+    }
+}
+
+
+/*
+                 当前类        同一package           子孙类             其他package
+ private           1               0                0                    0
+ protected         1               1                1                    0
+ public            1               1                1                    1
+
+ */
