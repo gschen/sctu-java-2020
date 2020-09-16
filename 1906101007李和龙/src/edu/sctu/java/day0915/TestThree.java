2f2de038baa97688866ebd863d98312d54d11f7d
lihelong1 <857788573@qq.com>
2020-09-16 11:58:26 +0800

    第三周第三次课

--- 1906101007李和龙/src/edu/sctu/java/day0915/TestThree.java
+++ 1906101007李和龙/src/edu/sctu/java/day0915/TestThree.java
@@ -6,7 +6,7 @@ public class TestThree extends TestOne {
     public static void main(String[] args) {
         TestThree chen = new TestThree();
         chen.name = "chen";
-        chen.sexy = "male";
+        chen.sexy = "male";  //这里报错说明privat在子孙类中也不能使用
         chen.age = 16;
     }
 }
