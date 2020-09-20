2043a73959831db7e2f63d9a124680339d7b5cdd
JohnDigger <541554971@qq.com>
2020-09-20 22:09:30 +0800

    作业

--- 1906101019+贾佳/src/edu/sctu/java/实验一/test6.java
+++ 1906101019+贾佳/src/edu/sctu/java/实验一/test6.java
@@ -3,24 +3,24 @@ package edu.sctu.java.实验一;
 import java.util.Scanner;
 
 //6、	编程输入学生的学习成绩的等级，给出相应的成绩范围。设A级为85分以上（包括85）；B级为70分以上（包括70分）；C级为60分以上（包括60分）；D级为60分以下。
-public class test6 {
-    public static void main(String[] args) {
-        Scanner Scan = new Scanner(System.in);
-        int grades = Scan.nextInt();
+import java.io.IOException;
 
-        if (grades>=85 && grades <=100){
-            System.out.println("A");
-        }else if (grades>=70 && grades <85){
-            System.out.println("B");
-        }else if (grades >=60 && grades <70){
-            System.out.println("C");
-        }else if(grades<60 && grades >=0){
-            System.out.println("D");
-        }else {
-            System.out.println("数据输入有误");
+public class test6 {
+    public static void main(String[] args) throws IOException {
+        char a;
+        a =(char)System.in.read();
+        String s = String.valueOf(a);
+        switch (s){
+            case "A" :
+                System.out.println("85分以上（包括85）");
+            case "B" :
+                System.out.println("70分以上（包括70分）");
+            case "C" :
+                System.out.println("60分以上（包括60分）");
+            case "D" :
+                System.out.println("60分以下");
+
         }
 
     }
-
-
 }
