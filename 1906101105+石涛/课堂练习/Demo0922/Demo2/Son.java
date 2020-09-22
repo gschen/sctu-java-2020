ea6d292217f0fad7989b9bb7c0dfe79900004be4
st13739438729 <1837211345@qq.com>
2020-09-22 11:59:33 +0800

    0922课堂练习代码提交

---
+++ 1906101105+石涛/课堂练习/Demo0922/Demo2/Son.java
@@ -0,0 +1,13 @@
+package Demo0922.Demo2;
+
+public class Son extends Parent{
+
+    // 重写
+    @Override // 注解 annotation：表示子类重写了父类的方法
+    void study() {
+        super.study();
+        super.study("father");
+
+        System.out.println("son正在学习");
+    }
+}
