bdad97a018aaa9afcbc374d3bf2f5be874572f9c
Gongsuo CHEN <2547253080@qq.com>
2020-09-22 14:39:42 +0800

    4-1

---
+++ 10001+张三/src/edu/sctu/java/day0922/Client1.java
@@ -0,0 +1,30 @@
+package edu.sctu.java.day0922;
+
+public class Client1 {
+
+    public static void main(String[] args) {
+//        Daught duoduo = new Daught();
+//        void dish () {
+//            System.out.println("女儿在洗碗");
+//        }
+//        duoduo.age = 10;
+//        duoduo.name = "duoduo";
+//        System.out.println(duoduo.dish());
+
+//        Daught a = new Daught();
+//        a.name = "aa";
+//        a.age = 10;
+//
+//        a.dish();
+//
+//
+
+        Mama mama = new Mama();
+        mama.dish();
+
+
+        Mama ma = new Daught();
+        ma.dish();
+
+    }
+}
---
+++ 10001+张三/src/edu/sctu/java/day0922/Daught.java
@@ -0,0 +1,9 @@
+package edu.sctu.java.day0922;
+
+public class Daught extends Mama {
+
+    @Override
+    void dish() {
+        System.out.println("daughter dish!");
+    }
+}
