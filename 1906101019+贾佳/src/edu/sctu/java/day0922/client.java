9361c21850fcf2d324d2e93488c147d57fe9e5db
JohnDigger <541554971@qq.com>
2020-09-22 19:10:07 +0800

    update

--- 1906101019+贾佳/src/edu/sctu/java/day0922/client.java
+++ 1906101019+贾佳/src/edu/sctu/java/day0922/client.java
@@ -1,6 +1,10 @@
 package edu.sctu.java.day0922;
 
 public class client {
+
+
+    private static Object Person;
+
     public static void main(String[] args) {
         Person per = new Student();
         per.eat();
@@ -12,5 +16,17 @@ public class client {
         per.sleep();
         per = new Doctor();
         per.sleep();
+
+        //向上转型
+        Student c = new Student();
+        Person a = c;
+
+        Person aa = new Student();
+        //向下转型
+
+        Student std = (Student) aa;
+
+        std.sleep();
+
     }
 }
