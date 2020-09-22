ba9f420975a86504df358dcd603ce668169bfacf
Frankenstein-st <862006205@qq.com>
2020-09-22 12:00:56 +0800

    课堂提交

---
+++ 1906101113+刘禹韬/src/edu/sctu/java/day0922/Client.java
@@ -0,0 +1,17 @@
+package edu.sctu.java.day0922;
+
+import edu.sctu.java.day0915.Extend.C;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+//        Child son = new Child();
+//        son.drank();
+
+        Father A = new Child();
+        //A.study();
+        A.drank();
+    }
+
+}
---
+++ 1906101113+刘禹韬/src/edu/sctu/java/day0922/Father.java
@@ -0,0 +1,22 @@
+package edu.sctu.java.day0922;
+
+public class Father {
+
+    String name;
+    int age;
+
+//    public Father(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+
+    public void drank(){
+        System.out.println("父亲经常喝酒");
+
+    }
+
+    public void study(){
+        System.out.println("父亲学习不上心");
+    }
+
+}
