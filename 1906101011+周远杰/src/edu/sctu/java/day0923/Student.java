2a4099cd116c59f02c3684b06689ac17998c8262
周远杰 <1308323582@qq.com>
2020-09-23 11:59:36 +0800

    第四周第三节课

---
+++ 1906101011+周远杰/src/edu/sctu/java/day0923/Student.java
@@ -0,0 +1,14 @@
+package edu.sctu.java.day0923;
+
+public class Student extends Person{
+
+    @Override
+    void rest(){
+        System.out.println("在上课，不可以休息！");
+    }
+
+    void work(int age){
+        System.out.println("学生在这个"+age+"岁都要学习");
+    }
+
+}
