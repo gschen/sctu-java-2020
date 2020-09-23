ac47b031b4e20077839057e318d0f20e00f74502
JohnDigger <541554971@qq.com>
2020-09-23 11:32:06 +0800

    update

---
+++ 1906101019+贾佳/src/edu/sctu/java/day0923/Person.java
@@ -0,0 +1,19 @@
+package edu.sctu.java.day0923;
+
+public class Person  implements PersonAction{
+
+    @Override
+    public void study() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void programming() {
+
+    }
+}
