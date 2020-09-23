c6e19ad4b3f3be67280ade2630bc4cb1594871df
rmang409 <278084531@qq.com>
2020-09-23 17:20:52 +0800

    1

--- 1709102008吕万金/src/dately/work/scool/experiment1/day0916/R10.java
+++ 1709102008吕万金/src/dately/work/scool/experiment1/day0916/R10.java
@@ -1,22 +1,53 @@
-//package dately.work.scool.experiment1.day0916;
-//
-//import java.util.ArrayList;
-//import java.util.HashSet;
-//import java.util.List;
-//
-//public class R10 {
-//    public static void main(String[] args) {
-//        int i,j,k;
+package dately.work.scool.experiment1.day0916;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class R10 {
+    int i,j;
+    int k=0;
+
+    void run(){
+        for (i=2;i<100;i++){
+            for (j=2;j<i;j++){
+                if (i%j==0){
+                    break;
+                }
+
+        }
+            if(j==i){
+                k++;
+                System.out.print(i+" ");
+                if (k%5==0){
+                    System.out.println("\n");
+                }}
+        }
+    }
+    public static void main(String[] args) {
+        R10 r10=new R10();
+        r10.run();
+
+
+    }
+
+
+    }
+//        int i,j;
+//        int k=0;
 //        for (i=2;i<=100;i++){
-//            for (j=2;j<=100;j++){
+//            for (j=2;j<i;j++){
 //                if (i%j==0){
 //                    break;
 //                }
-//            }if (){
-//                int k=0;
-//                k++;
 //            }
+//            if (j>=i){
+//                k++;
+//                System.out.print(i+" ");
+//                if (k%5==0){
+//                    System.out.println("\n");
+//            } }
 //        }
-//
 //    }
 //}
+
--- 1709102008吕万金/src/dately/work/scool/experiment1/day0916/R9.java
+++ 1709102008吕万金/src/dately/work/scool/experiment1/day0916/R9.java
@@ -1,37 +1,125 @@
 package dately.work.scool.experiment1.day0916;
 
-import java.util.Scanner;
 public class R9 {
+    int line;
+
+    void run() {
+        //从大写字母A开始
+        char A = 'A';
+        //循环的次数i=1代表第一行
+        int line = 4;
+        for (int i = 1; i <= line; i++) {
+            //打印空格
+            for (int k = 1; k <= (line - i); k++)
+                System.out.print(" ");
+            //打印字母
+            for (int j = 0; j < i; j++) {
+                System.out.print(A++);
+                System.out.print(" ");
+            }
+            //换行
+            System.out.println();
+        }
+        //逆向打印
+        for (int i = line; i >= 1; i--) {
+            //打印空格
+            for (int k = 1; k <= (line - i); k++)
+                System.out.print(" ");
+            //打印字母
+            for (int j = 0; j < i; j++) {
+                System.out.print(A++);
+                System.out.print(" ");
+            }
+            //换行
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        char s = 'A';
-        Scanner input = new Scanner(System.in);
-        System.out.println("请输入您要打印的行数:");
-        int num = input.nextInt();
-        for (int i = 1; i <= num; i++) {
-            for (int j = 0; j < (num - i); j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= (i * 2 - 1); j++) {
-                System.out.print(s);
-                int v = (int) s;
-                v++;
-                s = (char) v;
-            }
-            System.out.println();
-
-        }
-        for (int i = num - 1; i > 0; i--) {
-            for (int j = 0; j < num - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < i * 2 - 1; j++) {
-                System.out.print(s);
-                int v = (int) s;
-                v++;
-                s = (char) v;
-            }
-            System.out.println();
-        }
+        R9 text=new R9();
+        text.line=4;
+        text.run();
     }
-    }
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //        char s = 'A';
+//        int num = 4;
+//        for (int i = 1; i <= num; i++) {
+//            for (int j = 0; j < (num - i); j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 1; j <= (i*2 - 1); j++) {
+//                System.out.print(s);
+//                int v = (int) s;
+//                v++;
+//                s = (char) v;
+//            }
+//            System.out.println();
+//
+//        }
+//        for (int i = num - 1; i > 0; i--) {
+//            for (int j = 0; j < num - i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j < i * 2 - 1; j++) {
+//                System.out.print(s);
+//                int v = (int) s;
+//                v++;
+//                s = (char) v;
+//            }
+//            System.out.println();
+//        }
+//    }
+//    }
+//        int num = 4;//代表bai上面有du4行zhi，总共dao是7行
+//        for (int i = 0; i < num; i++) {
+//            for (int j = 0; j < num - 1 - i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j < 2 * i + 1; j++) {
+//                if (j == 2 * i) {
+//                    System.out.println("*");
+//                } else if (i == num - 1 && j == i) {
+//                    System.out.print("+");
+//                } else {
+//                    System.out.print("*");
+//                }
+//            }
+//        }
+//        for (int i = num - 2; i >= 0; i--) {
+//            for (int j = 0; j < num - 1 - i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j < 2 * i + 1; j++) {
+//                if (j == 2 * i) {
+//                    System.out.println("*");
+//                } else {
+//                    System.out.print("*");
+//                }
+//            }
+//        }
+//    }
+//}
