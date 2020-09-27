2dd01ec4a84bcaf3115a4e90a92781cd95f12a53
rmang409 <278084531@qq.com>
2020-09-27 19:10:13 +0800

    1

---
+++ 1709102008吕万金/src/edu/sctu/java/day0927/PersonTest.java
@@ -0,0 +1,18 @@
+package edu.sctu.java.day0927;
+
+public class PersonTest {
+
+    public static void main(String[] args) {
+        int size=3;
+        String[] persons=new String[size];
+        persons[0]="zhang";
+        persons[1]="san";
+        persons[2]="li";
+
+//        for (int i = 0; i < 3; i++) {
+//            persons[i]=new Person();
+//
+//        }
+
+    }
+}
---
+++ 1709102008吕万金/src/edu/sctu/java/day0927/StringExample.java
@@ -0,0 +1,13 @@
+package edu.sctu.java.day0927;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String name=new String("hello,world!");
+//        int n=name.length();
+//        System.out.println(n);
+        String k=name.indent(5);
+
+        System.out.println(k);
+
+    }
+}
