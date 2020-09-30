4be847780e8af7f259cf8ab947ae6c8f59a878be
lk <963385794@qq.com>
2020-09-30 11:52:03 +0800

    测试

---
+++ 1906101049+李凯/src/课堂练习01/Java/Firm.java
@@ -0,0 +1,11 @@
+package 课堂练习01.Java;
+
+public class Firm {
+    String position;
+    void work1(){
+        System.out.println("我负责软件开发");
+    }
+    void work2(){
+        System.out.println("我负责网页前端");
+    }
+}
---
+++ 1906101049+李凯/src/课堂练习03/java/Person.java
@@ -0,0 +1,10 @@
+package 课堂练习03.java;
+
+public class Person {
+    String name;
+    int age;
+    void learn(){
+        System.out.println("振奋");
+    }
+
+}
